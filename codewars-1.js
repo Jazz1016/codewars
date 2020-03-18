d33930a42f16a6ff4151d7dbc3c6397080f70ce7
Jazz1016 <james.q.lea@gmail.com>
2020-03-18 13:42:01 -0600

    solved another 6

--- codewars-1.js
+++ codewars-1.js
@@ -212,3 +212,56 @@ var maxSequence = function(arr) {
     }, 0);
   }
 };
+
+// In this kata, you must create a digital root function.
+
+// A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+// Here's how it works:
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+function digital_root(n) {
+  // ...
+  const runIt = n => {
+    n = n.toString();
+    let numArr = n.split("");
+    numArr = numArr.map(el => {
+      return +el;
+    });
+    console.log(numArr);
+
+    let total = numArr.reduce((acc, el) => {
+      return acc + el;
+    });
+    console.log(total);
+    return total;
+  };
+  if (runIt(n) >= 10) {
+    return runIt(runIt(n));
+  } else {
+    return runIt(n);
+  }
+}
