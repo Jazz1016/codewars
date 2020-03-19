eaac280d0d7a2ce2b100ce9bc0cd449fde848b17
Jazz1016 <james.q.lea@gmail.com>
2020-03-19 10:48:16 -0600

    couple more probs done

--- codewars-1.js
+++ codewars-1.js
@@ -265,3 +265,50 @@ function digital_root(n) {
     return runIt(n);
   }
 }
+
+// Your task is to find all the elements of an array that are non consecutive.
+
+// A number is non consecutive if it is not exactly one larger than the previous element in the array. The first element gets a pass and is never considered non consecutive.
+
+// Create a function named allNonConsecutive
+
+// E.g., if we have an array [1,2,3,4,6,7,8,10] then 6 and 10 are non-consecutive.
+
+// You should return the results as an array of objects with two values i: <the index of the non-consecutive number> and n: <the non-consecutive number>.
+
+// E.g., for the above array the result should be:
+
+// [
+//   {i: 4, n:6},
+//   {i: 7, n:10}
+// ]
+// If the whole array is consecutive or has one element then return an empty array.
+
+// The array elements will all be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive and/or negetive.
+
+// If you like this kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+function allNonConsecutive(arr) {
+  console.log(arr);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] + 1 === arr[i + 1] || arr[i + 1] === undefined) {
+    } else {
+      newArr.push({ i: i + 1, n: arr[i + 1] });
+    }
+  }
+  return newArr;
+}
+
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+
+function differenceInAges(ages) {
+  ages = ages.sort((a, b) => {
+    return a - b;
+  });
+  console.log(ages);
+  let dif = ages[ages.length - 1] - ages[0];
+  return [ages[0], ages[ages.length - 1], dif];
+}
