819b106cd40640af9fe92541fa357fd5c2fee710
Jazz1016 <james.q.lea@gmail.com>
2020-03-27 09:37:19 -0600

    a couple probs done

---
+++ cordewars-2.js
@@ -0,0 +1,47 @@
+// Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you do, but exactly how many? Maybe it's time to write a program to figure it out.
+
+// You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+// (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+// Note: Of course you can't do half a fold. You should know what this means ;P
+
+// Also, if somebody is giving you a negative distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language. In Shell please return None).
+
+function foldTo(distance) {
+  var counter = 0;
+
+  if (distance < 0) {
+    return null;
+  }
+
+  for (var i = 0.0001; i < distance; i) {
+    i *= 2;
+    counter += 1;
+  }
+
+  return counter;
+}
+
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// isSquare(-1) returns  false
+// isSquare(0) returns   true
+// isSquare(3) returns   false
+// isSquare(4) returns   true
+// isSquare(25) returns  true
+// isSquare(26) returns  false
+
+var isSquare = function(n) {
+  return Number.isInteger(Math.sqrt(n));
+};
