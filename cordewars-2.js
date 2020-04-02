70bc798b2275a36ebf9edd318f0773a12f935294
Jazz1016 <james.q.lea@gmail.com>
2020-04-01 23:43:20 -0600

    good

--- cordewars-2.js
+++ cordewars-2.js
@@ -51,3 +51,30 @@ var isSquare = function(n) {
 function multiply(a, b) {
   return a * b;
 }
+
+// Write a function that accepts two arguments: an array/list of integers and another integer (n).
+
+// Determine the number of times where two integers in the array have a difference of n.
+
+// For example:
+
+// [1, 1, 5, 6, 9, 16, 27], n=4  -->  3  # (1,5), (1,5), (5,9)
+// [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
+// FUNDAMENTALS
+
+const intDiff = (arr, n) => {
+  console.log(arr);
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(arr[i], arr[j], n, count);
+      if (arr[i] - arr[j] === n) {
+        count++;
+      } else if (arr[j] - arr[i] === n) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+  return count;
+};
