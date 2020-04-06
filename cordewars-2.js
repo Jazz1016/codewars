cc575136331d7eb62097c695069daedc73476d1f
Jazz1016 <james.q.lea@gmail.com>
2020-04-06 17:28:44 -0600

    another prob solved

--- cordewars-2.js
+++ cordewars-2.js
@@ -233,3 +233,25 @@ function simplify(number) {
   return number.join("");
   console.log(number);
 }
+
+// Cheesy Cheeseman just got a new monitor! He is happy with it, but he just discovered that his old desktop wallpaper no longer fits. He wants to find a new wallpaper, but does not know which size wallpaper he should be looking for, and alas, he just threw out the new monitor's box. Luckily he remembers the width and the aspect ratio of the monitor from when Bob Mortimer sold it to him. Can you help Cheesy out?
+
+// The Challenge
+// Given an integer width and a string ratio written as WIDTH:HEIGHT, output the screen dimensions as a string written as WIDTHxHEIGHT.
+function findScreenHeight(width, ratio) {
+  ratio = ratio.split("");
+  console.log(width, ratio);
+  let colonIndex = 0;
+  for (let i = 0; i < ratio.length; i++) {
+    if (ratio[i] === ":") {
+      colonIndex = i;
+    }
+  }
+  //     console.log(colonIndex)
+  let divide = ratio.slice(0, colonIndex);
+  let multiply = ratio.slice(colonIndex + 1, ratio.length + 1);
+  multiply = multiply.join("");
+  divide = divide.join("");
+  console.log(multiply, divide);
+  return `${width}x${(width * multiply) / divide}`;
+}
