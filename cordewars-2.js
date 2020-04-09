82c25b5cb12b6ec9dfb26b03f9585fec37a4d1ed
Jazz1016 <james.q.lea@gmail.com>
2020-04-08 22:25:56 -0600

    message

--- cordewars-2.js
+++ cordewars-2.js
@@ -255,3 +255,25 @@ function findScreenHeight(width, ratio) {
   console.log(multiply, divide);
   return `${width}x${(width * multiply) / divide}`;
 }
+
+// Is the string uppercase?
+// Task
+// Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+// "c".isUpperCase() == false
+// "C".isUpperCase() == true
+// "hello I AM DONALD".isUpperCase() == false
+// "HELLO I AM DONALD".isUpperCase() == true
+// "ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
+// "ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function () {
+  console.log(this, this.toUpperCase());
+  if (this === this.toUpperCase()) {
+    console.log("hit");
+    return true;
+  } else {
+    return false;
+  }
+};
