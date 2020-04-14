891276318b02ac6ea9939fb4eabc5f5e15ba2cab
Jazz1016 <james.q.lea@gmail.com>
2020-04-14 15:20:37 -0600

    kata 7 solved

--- codewars-3.js
+++ codewars-3.js
@@ -8,3 +8,29 @@ function getMinMax(arr) {
   console.log(arr);
   return [arr[0], arr[arr.length - 1]];
 }
+
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ....
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  // console.log(n)
+  let total = 0;
+  function looper(n) {
+    console.log(total, n);
+    total = total + n;
+    if (n / 2 > 0) {
+      n = Math.floor(n / 2);
+      looper(n);
+    } else {
+      return n;
+    }
+  }
+  looper(n);
+  return total;
+}
