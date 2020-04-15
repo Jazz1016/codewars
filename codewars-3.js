01a0f506809985d183851b424949ef84e3e10730
Jazz1016 <james.q.lea@gmail.com>
2020-04-15 15:45:51 -0600

    kata 7 completed

--- codewars-3.js
+++ codewars-3.js
@@ -34,3 +34,34 @@ function halvingSum(n) {
   looper(n);
   return total;
 }
+
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the results.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+// My other katas
+// If you enjoyed this kata then please try my other katas! :-)
+
+// Translations are welcome!
+
+function myLanguages(results) {
+  console.log(results);
+  let arr = [];
+  for (let key in results) {
+    console.log(key, results[key]);
+    if (results[key] >= 60) {
+      arr.push([key, results[key]]);
+    }
+  }
+  arr = arr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  console.log(arr);
+  return arr.map((el) => {
+    return el[0];
+  });
+}
