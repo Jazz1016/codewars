819e69a324d32bbb37da7055e50ef88dc500683a
Jazz1016 <james.q.lea@gmail.com>
2020-04-17 14:52:09 -0600

    kata lvl 6 done

--- codewars-3.js
+++ codewars-3.js
@@ -95,3 +95,34 @@ function calculate(num1, operation, num2) {
       return null;
   }
 }
+
+// The objective is to return all pairs of integers from a given collection of integers that have a difference of 2.
+
+// The result should be sorted in ascending order.
+
+// The input will consist of unique values. The order of the integers in the input collection should not matter.
+
+// Examples
+// [1, 2, 3, 4]      -->  [[1, 3], [2, 4]]
+// [4, 1, 2, 3]      -->  [[1, 3], [2, 4]]
+// [1, 23, 3, 4, 7]  -->  [[1, 3]]
+// [4, 3, 1, 5, 6]   -->  [[1, 3], [3, 5], [4, 6]]
+
+function twosDifference(input) {
+  console.log(input);
+  let output = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      //      console.log(input[i], input[j])
+      if (input[i] - input[j] === 2) {
+        output.push([input[j], input[i]]);
+      } else if (input[j] - input[i] === 2) {
+        output.push([input[i], input[j]]);
+      }
+    }
+  }
+  //  console.log(output)
+  return output.sort((a, b) => {
+    return a[0] - b[0];
+  });
+}
