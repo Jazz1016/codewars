7b8892c7ed734c9c980f592d8940a9da3865318b
Jazz1016 <james.q.lea@gmail.com>
2020-04-22 17:10:29 -0600

    kata 7 solved

--- codewars-3.js
+++ codewars-3.js
@@ -234,3 +234,36 @@ function multiplyAndFilter(array, multiplier) {
     });
   }
 }
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Example:
+
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers) {
+  // ...
+  console.log(numbers);
+  let numArr = [];
+  strNumArr = numbers.split(" ");
+  numArr = strNumArr.map((el) => {
+    return parseInt(el);
+  });
+  console.log(numArr);
+  sortedArr = numArr.sort(function (a, b) {
+    return b - a;
+  });
+  console.log(sortedArr);
+  let sortedArr1 = [...sortedArr];
+  //   console.log(sortedArr.shift())
+  //   console.log(sortedArr.pop())
+
+  return `${sortedArr1.shift()} ${sortedArr.pop()}`;
+}
