2b59984ec92f2d344b35079e9ca15e63789b8af2
Jazz1016 <james.q.lea@gmail.com>
2020-04-23 13:56:53 -0600

    lvl 5 codewars reachedgit add .!

--- codewars-3.js
+++ codewars-3.js
@@ -284,3 +284,38 @@ function getSumOfDigits(integer) {
   }
   return sum;
 }
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+function findOutlier(integers) {
+  console.log(integers);
+  let oddCount = 0;
+  let evenCount = 0;
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  }
+  for (let j = 0; j < integers.length; j++) {
+    console.log(oddCount, evenCount);
+    if (oddCount < evenCount) {
+      if (integers[j] % 2 !== 0) {
+        console.log("hit", integers[j]);
+        return integers[j];
+      }
+    } else {
+      if (integers[j] % 2 === 0) {
+        return integers[j];
+      }
+    }
+  }
+}
