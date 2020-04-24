0c8b55d59b5034054b02fc9d229557f41b3896db
Jazz1016 <james.q.lea@gmail.com>
2020-04-24 15:51:55 -0600

    kata 7 completed

--- codewars-3.js
+++ codewars-3.js
@@ -319,3 +319,34 @@ function findOutlier(integers) {
     }
   }
 }
+// In the following 6 digit number:
+
+// 283910
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+// Adapted from ProjectEuler.net
+
+function solution(digits) {
+  let large = 0;
+  digits = digits.split("");
+  console.log(digits);
+  for (let i = 0; i < digits.length; i++) {
+    if (
+      +`${digits[i]}${digits[i + 1]}${digits[i + 2]}${digits[i + 3]}${
+        digits[i + 4]
+      }` > large
+    ) {
+      large = +`${digits[i]}${digits[i + 1]}${digits[i + 2]}${digits[i + 3]}${
+        digits[i + 4]
+      }`;
+    }
+  }
+  return large;
+}
