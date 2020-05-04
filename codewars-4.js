3c5557923519715f8eb1dfb0f3054f995bc47167
Jazz1016 <james.q.lea@gmail.com>
2020-05-04 10:17:32 -0600

    kata 7 done

--- codewars-4.js
+++ codewars-4.js
@@ -27,3 +27,27 @@ function getMiddle(s) {
     return `${s[Math.floor(s.length / 2)]}`;
   }
 }
+
+// Given a list of numbers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Example:
+// odd_or_even([0])          ==  "even"
+// odd_or_even([0, 1, 4])    ==  "odd"
+// odd_or_even([0, -1, -5])  ==  "even"
+
+function oddOrEven(array) {
+  console.log(array);
+  let total = array.reduce((acc, el) => {
+    return acc + el;
+  }, 0);
+  console.log(total);
+  if (total % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
