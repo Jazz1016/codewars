898d7ff6c8bb7e353e54b28b2a056378ea7fea81
Jazz1016 <james.q.lea@gmail.com>
2020-05-11 22:23:30 -0600

    kata 7 done

--- codewars-4.js
+++ codewars-4.js
@@ -178,3 +178,41 @@ function removeSmallest(numbers) {
     }
   }
 }
+
+// Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+
+// Examples:
+
+// [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+// [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+// Points:
+
+// The arrays may be of different lengths, with at least one character/digit.
+// One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+// Inspired by https://adriann.github.io/programming_problems.html
+
+function mergeArrays(a, b) {
+  console.log(a,b)
+  let arr = []
+  if(a.length >= b.length){
+  for(let i = 0; i < a.length; i++){
+    if(a[i] !== undefined){
+    arr.push(a[i])
+    }
+    if(b[i] !== undefined){
+    arr.push(b[i])
+    }
+  }
+  return arr
+  } else {
+  for(let i = 0; i < b.length; i++){
+    if(a[i] !== undefined){
+    arr.push(a[i])
+    }
+    if(b[i] !== undefined){
+    arr.push(b[i])
+    }
+  }
+  return arr
+  }
