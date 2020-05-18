097b5a754caae327610e59836c42d094b02ea746
Jazz1016 <james.q.lea@gmail.com>
2020-05-18 15:11:56 -0600

    kata lvl 7

--- codewars-4.js
+++ codewars-4.js
@@ -317,4 +317,52 @@ function towerBuilder(nFloors) {
     return arr
   }
 
-  fdsaf
+//  Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples
+// basicOp('+', 4, 7)         // Output: 11
+// basicOp('-', 15, 18)       // Output: -3
+// basicOp('*', 5, 5)         // Output: 25
+// basicOp('/', 49, 7)        // Output: 7
+
+function basicOp(operation, value1, value2)
+{
+  switch(operation){
+    case '+':
+    return value1 + value2
+    break;
+    case '-':
+    return value1 - value2
+    break;
+    case '*':
+    return value1 * value2
+    break;
+    case '/':
+    return value1 / value2
+    break;
+  }
+}
+
+// You are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
+
+// Your Task
+// Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+
+// Rules
+// you can put at most 8 eggs into the pot at once
+// it takes 5 minutes to boil an egg
+// we assume, that the water is boiling all the time (no time to heat up)
+// for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+// Example
+// cookingTime(0); // must return 0
+// cookingTime(5); // must return 5
+// cookingTime(10); // must return 10
+
+function cookingTime(eggs) {
+  let time = 0
+  return time + Math.ceil(eggs/8) * 5
+    console.log(eggs)
+  }
