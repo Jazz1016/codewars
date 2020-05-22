61abca88346fc793f3f03c64e7b4e0967c517a23
Jazz1016 <james.q.lea@gmail.com>
2020-05-21 23:37:25 -0600

    kata 7

--- codewars-4.js
+++ codewars-4.js
@@ -383,3 +383,26 @@ function cookingTime(eggs) {
 function getNames(data){
   return data.map((item)=>{return item.name});
 }
+
+// Happy Holidays fellow Code Warriors!
+// Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+// Sort Santa's Reindeer
+// Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+// Notes:
+// It's guaranteed that each string is composed of two words
+// In case of two identical last names, keep the original order
+
+function sortReindeer(reindeerNames) {
+  for(let i = 0; i < reindeerNames.length; i++){
+    reindeerNames[i] = reindeerNames[i].split(' ')
+  }
+    reindeerNames =  reindeerNames.sort()
+  console.log(reindeerNames)
+    
+    for(let i = 0; i < reindeerNames.length; i++){
+    reindeerNames[i] = reindeerNames[i].join(' ')
+  }
+  return reindeerNames
+  }
