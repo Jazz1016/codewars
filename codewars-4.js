ccb26f1802cc3ed4fc7ebc5a81fb335c58e5d9eb
Jazz1016 <james.q.lea@gmail.com>
2021-01-16 18:04:19 -0700

    hopefully i dont break this

--- codewars-4.js
+++ codewars-4.js
@@ -451,3 +451,63 @@ function rowWeights(array){
   }
   return arr
 }
+
+// Create a function that returns a villain name based on the user's birthday. The birthday will be passed to the function as a valid Date object, so for simplicity, there's no need to worry about converting strings to dates.
+
+// The first name will come from the month, and the last name will come from the last digit of the date:
+
+// Month -> first name
+
+// January -> "The Evil"
+// February -> "The Vile"
+// March -> "The Cruel"
+// April -> "The Trashy"
+// May -> "The Despicable"
+// June -> "The Embarrassing"
+// July -> "The Disreputable"
+// August -> "The Atrocious"
+// September -> "The Twirling"
+// October -> "The Orange"
+// November -> "The Terrifying"
+// December -> "The Awkward"
+// Last digit of date -> last name
+
+// 0 -> "Mustache"
+// 1 -> "Pickle"
+// 2 -> "Hood Ornament"
+// 3 -> "Raisin"
+// 4 -> "Recycling Bin"
+// 5 -> "Potato"
+// 6 -> "Tomato"
+// 7 -> "House Cat"
+// 8 -> "Teaspoon"
+// 9 -> "Laundry Basket"
+// The returned value should be a string in the form of "First Name Last Name".
+
+// For example, a birthday of November 18 would return "The Terrifying Teaspoon"
+
+function getVillainName(birthday){
+  const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"];
+  const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"];
+  let villianFirstName
+  let villianLastName
+  console.log(
+//     birthday.getUTCMonth(),
+    birthday.getUTCDate()
+  )
+  for(let i = 0; i<m.length; i++){
+    if(i === birthday.getMonth()){
+      villianFirstName = m[i]
+    }
+  }
+ let dateArray = birthday.getDate().toString().split('')
+ 
+ console.log(dateArray)
+  
+  for(let i = 0; i<d.length; i++){
+    if(i === +dateArray[dateArray.length - 1]){
+      villianLastName = d[i]
+    }
+  }
+  return `The ${villianFirstName} ${villianLastName}`
+}
