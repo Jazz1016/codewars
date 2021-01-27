804dec98bd917025a0f5a4f7953f1e62d2b8bd50
Jazz1016 <james.q.lea@gmail.com>
2021-01-27 09:52:47 -0700

    kata 6 done

--- codewars-4.js
+++ codewars-4.js
@@ -605,3 +605,30 @@ function wordsToMarks(string){
   }
   return counter
 }
+
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+// Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+function iqTest(numbers){
+  let numbersArr = numbers.split(" ")
+  let evensArr = []
+  let oddsArr = []
+  let oddsIndex = 1
+  let evensIndex = 1
+  
+  for(let i = 0; i < numbersArr.length; i++){
+    if(+numbersArr[i] % 2 === 0){
+        evensArr.push(numbersArr)
+        evensIndex += i
+    } else {
+      oddsArr.push(numbersArr[i])
+      oddsIndex += i
+    }
+  }
+  if(evensArr.length > oddsArr.length){
+    return oddsIndex
+  } else {
+    return evensIndex
+  }
+}
