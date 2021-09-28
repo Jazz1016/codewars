995a59baebd74fa9f7e5268014c374bc740b6162
James L <59751464+Jazz1016@users.noreply.github.com>
2021-09-28 09:31:29 -0600

    kata lvl 8 completed

--- codewars-5.js
+++ codewars-5.js
@@ -50,3 +50,9 @@ function greet(name){
     return "Hello, " + name + "!";
   }
 }
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWords(str){  
+  return str.split(" ").reverse().join(" ")
+}
