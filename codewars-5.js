b93f75a291e13c12f7fafd0ee450a922bdda32cb
James L <59751464+Jazz1016@users.noreply.github.com>
2021-12-05 17:25:02 -0700

    kata lvl 7 completed

--- codewars-5.js
+++ codewars-5.js
@@ -290,15 +290,39 @@ function evenOrOdd(str) {
 function sumDigits(number) {
   let str = `${number}`;
   if (number < 0) {
-    console.log(str.substring(1))
-    str = str.substring(1)
+    console.log(str.substring(1));
+    str = str.substring(1);
   }
-  
+
   let num = 0;
   for (let i = 0; i < str.length; i++) {
-    console.log(number, parseInt(str[i]))
+    console.log(number, parseInt(str[i]));
     num += parseInt(str[i]);
   }
-  console.log(num)
+  console.log(num);
   return num;
 }
+
+// The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+
+// In this kata we will recreate that stunt... sort of.
+
+// You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+
+// The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+
+// Return the resulting lamps string. See example tests for more clarity.
+
+function flyBy(lamps, drone) {
+  let location = "";
+  for (let i = 0; i < lamps.length; i++) {
+    if (i < drone.length) {
+      location += "o";
+    } else {
+      location += "x";
+    }
+  }
+  return location;
+}
+
+flyBy(("xxxxxx", "====T"), "ooooox");
